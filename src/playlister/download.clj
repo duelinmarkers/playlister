6be5cd3c3d8d044b3b1571b68e8a6a353ec49037
John Hume <duelin.markers@gmail.com>
2016-04-08 16:45:13 -0500

    Pretty sure that's equivalent.

--- src/playlister/download.clj
+++ src/playlister/download.clj
@@ -7,7 +7,6 @@
 
 (def song-start-end-time-format (DateTimeFormatter/ofPattern "MM/dd/yyyy HH:mm:ss"))
 (def filename-date-time-format (DateTimeFormatter/ofPattern "yyyy-MM-dd-HHmmss"))
-(def request-datestamp-format (DateTimeFormatter/ofPattern "YYYY-MM-dd'T'HH:mm:ss"))
 
 (defn parse-time [^CharSequence s]
   (LocalDateTime/parse s song-start-end-time-format))
@@ -18,7 +17,7 @@
 (defn tracks-url [{:keys [datestamp limit]}]
   (str "https://api.composer.nprstations.org/v1/widget/50ef24ebe1c8a1369593d032/tracks?format=json"
        "&datestamp=" (if (instance? LocalDateTime datestamp)
-                       (.format datestamp request-datestamp-format)
+                       (.format datestamp DateTimeFormatter/ISO_LOCAL_DATE_TIME)
                        datestamp)
        "&limit=" limit
        "&hide_itunes=true"))
